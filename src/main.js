c8f43159ccf9f5e7d55aa9264cdf502741601823
fishfinger-media <111457292+Rahuli20@users.noreply.github.com>
2024-04-09 17:27:15 +0100

    experimenting

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 import './styles/style.css'
 
-console.log('Hello')
+import gsap from 'gsap'
+
+let slideOne = document.querySelector('.slide.is-1')
+let slideTwo = document.querySelector('.slide.is-2')
+
+
+
+// gsap timline 
+
+const tl = gsap.timeline({})
+
+tl.to(slideOne, {
+    x: '-100%',
+    duration: 3,
+    ease: 'power4.inOut'
+})
+
+tl.to('.overlay', {
+    x: '-100%',
+    duration: 3.5,
+    backgroundColor: '#FFEBEE',
+    ease: 'power4.inOut'
+
+}, '-=3.1')
+
+
+tl.to(slideTwo, {
+    x: '-0%',
+    duration: 2,
+    ease: 'power4.inOut',
+}, '-=2.5')

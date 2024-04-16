edf771d821cf210b288b5f2e924d18515ad19bbc
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-04-16 23:01:56 +0100

    Custom Slider

--- src/main.js
+++ src/main.js
@@ -2,44 +2,46 @@ import './styles/style.css'
 
 import gsap from 'gsap'
 
-const goToCakeButton = document.querySelector('.gotocake');
-const goToBurgerButton = document.querySelector('.gotoburger');
+const slides = ['burger', 'cake', 'pizza', 'sushi'];
 
+const slideColors = {
+    burger: '#A6FAFE',
+    cake: '#FDF6F7',
+    pizza: '#FAE3CD',
+    sushi: '#F1F3A6'
+};
 
+function transitionSlide(fromSlide, toSlide, direction) {
+    const fromSlideSelector = `[data-slide="${fromSlide}"]`;
+    const toSlideSelector = `[data-slide="${toSlide}"]`;
+    const fromSlideImgSelector = `[data-slide-img="${fromSlide}"]`;
+    const toSlideImgSelector = `[data-slide-img="${toSlide}"]`;
 
-goToCakeButton.addEventListener('click', function () {
-  
-    let moveNext = gsap.timeline();
+    const backgroundColorFrom = slideColors[fromSlide];
+    const backgroundColorTo = slideColors[toSlide];
 
-    moveNext.to('.hero_slide-fader', { x: "-100%", duration: 3, ease: 'power4.inOut',  backgroundColor: '#ffd9e0' })
-    .to('[data-slide="1"]', {
-        x: "-100%",
-        z: "1",
-        duration:2,
-        ease:'power4.inOut',
-        stagger: {
-            amount: 0.05,
-            from: "random"
-        },
-    }, "<0.5")
-    .from('[data-slide="2"]', {x:"100%", duration: 2, ease: 'power4.inOut', stagger:{amount: 0.05, from: 'random'}}, "<")
+    gsap.timeline()
+        .set('.slider_fade', { xPercent: direction === 'next' ? 0 : -200, backgroundColor: backgroundColorFrom })
+        .set(toSlideSelector, { xPercent: direction === 'next' ? 100 : -100 })
+        .set(fromSlideImgSelector, { x: '0%', z: 1 })
+        .set(toSlideImgSelector, { x: direction === 'next' ? '10%' : '-10%', z: 1 })
 
+        .to('.slider_fade', { xPercent: direction === 'next' ? -200 : 0, duration: 3, ease: "power4.inOut", backgroundColor: backgroundColorTo })
+        .to(fromSlideSelector, { xPercent: direction === 'next' ? -100 : 100, duration: 2, ease: "power4.inOut" }, "<0.5")
+        .to(fromSlideImgSelector, { x: direction === 'next' ? '-10%' : '10%', z: 1, duration: 2, ease: "power4.inOut", stagger: { amount: 0.5, from: "random" } }, "<")
+        .to(toSlideSelector, { xPercent: 0, duration: 2, ease: "power4.inOut" }, "<")
+        .to(toSlideImgSelector, { x: '0%', z: 1, duration: 1.5, ease: "power4.inOut", stagger: { amount: 0.5, from: "random" } }, "<");
+}
+
+slides.forEach((slide, index) => {
+    const nextSlide = slides[(index + 1) % slides.length];
+    const prevSlide = slides[(index - 1 + slides.length) % slides.length];
+
+    document.querySelector(`[goToSlide="${nextSlide}"][slide-direction="next"]`).addEventListener('click', () => {
+        transitionSlide(slide, nextSlide, 'next');
+    });
+
+    document.querySelector(`[goToSlide="${prevSlide}"][slide-direction="prev"]`).addEventListener('click', () => {
+        transitionSlide(slide, prevSlide, 'prev');
+    });
 });
-
-
-goToBurgerButton.addEventListener('click', function () {
-    
-    let moveNext = gsap.timeline();
-
-    moveNext.to('.hero_slide-fader', { x: "100%", duration: 3, ease: 'power4.inOut',  backgroundColor: '#9CF9FF' })
-    .to('[data-slide="1"]', {
-        x: "0%",
-        z: "1",
-        duration:2,
-        ease:'power4.inOut',
-        stagger: {
-            amount: 0.05,
-            from: "random"
-        },
-    }, "<0.5")
-})

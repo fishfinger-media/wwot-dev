1c2cfed6e14862c84d5ad8a49965dacdf89b0b6b
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-04-25 19:40:17 +0100

    class change

--- src/main.js
+++ src/main.js
@@ -24,12 +24,12 @@ function transitionSlide(fromSlide, toSlide, direction) {
     const backgroundColorTo = slideColors[toSlide];
 
     gsap.timeline()
-        .set('.slide_fade', { xPercent: direction === 'next' ? 0 : -200, backgroundColor: backgroundColorFrom })
+        .set('.slider_fade', { xPercent: direction === 'next' ? 0 : -200, backgroundColor: backgroundColorFrom })
         .set(toSlideSelector, { xPercent: direction === 'next' ? 100 : -100 })
         .set(fromSlideImgSelector, { x: '0%', z: 1 })
         .set(toSlideImgSelector, { x: direction === 'next' ? '10%' : '-10%', z: 1 })
 
-        .to('.slide_fade', { xPercent: direction === 'next' ? -200 : 0, duration: 3, ease: "power4.inOut", backgroundColor: backgroundColorTo })
+        .to('.slider_fade', { xPercent: direction === 'next' ? -200 : 0, duration: 3, ease: "power4.inOut", backgroundColor: backgroundColorTo })
         .to(fromSlideSelector, { xPercent: direction === 'next' ? -100 : 100, duration: 2, ease: "power4.inOut" }, "<0.5")
         .to(fromSlideImgSelector, { x: direction === 'next' ? '-10%' : '10%', z: 1, duration: 2, ease: "power4.inOut", stagger: { amount: 0.5, from: "random" } }, "<")
         .to(toSlideSelector, { xPercent: 0, duration: 2, ease: "power4.inOut" }, "<")

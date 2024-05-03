08cd10354f3a9fe8ac1c7f3a49b9ee48cc36dba4
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-05-03 11:29:12 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1010,7 +1010,7 @@ function animateBoatAlt() {
 animateBoatAlt();
 
 // NAVIGATION
-
+if (window.matchMedia("(max-width: 991px)").matches) {
     let navSize = gsap.timeline({
         scrollTrigger: {
             trigger: '.navigation',
@@ -1022,7 +1022,7 @@ animateBoatAlt();
     navSize.to('.nav_logo',{ height: '3.5rem', duration: 1, ease: "power4.inOut",})
     navSize.to('.nav_content', { paddingTop: '2rem', paddingBottom: '2rem', duration: 1, ease: "power4.inOut" }, "-=1")
     navSize.to('.navigation', { background: 'rgba(245, 254, 255, 0.2)', backdropFilter: 'blur(5px)', duration: 1, ease: "power4.inOut" }, "-=1")
-
+}
 
 
 
